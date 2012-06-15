c37e173c776395c0f7bef57ae6f333e61ed86055
Mat Alan Gilbert <matalangilbert@gmail.com>
2012-06-15 11:41:42 +0100

    Post to Rails API endpoint

---
+++ poster.rb
@@ -0,0 +1,29 @@
+# If you post to a Ruby on Rails REST API endpoint, then you'll get an
+# InvalidAuthenticityToken exception unless you set a different
+# content type in the request headers, since any post from a form must
+# contain an authenticity token.
+#
+# This example shows you how to post to a rails endpoint.
+
+class Poster
+  require 'json'
+  require 'uri'
+  require 'net/http'
+
+  ENDPOINT = 'http://localhost:3000/messages/'
+  #ENDPOINT = 'http://hollow-flower-9984.herokuapp.com/messages'
+  def post_to_endpoint(text)
+    uri = URI.parse(ENDPOINT)
+  
+    post_params = { :content => text }
+  
+    # Convert the parameters into JSON and set the content type as application/json
+    req = Net::HTTP::Post.new(uri.path)
+    req.body = JSON.generate(post_params)
+    req["Content-Type"] = "application/json"
+    
+    http = Net::HTTP.new(uri.host, uri.port)
+    response = http.start {|htt| htt.request(req)}
+  end
+end
+
